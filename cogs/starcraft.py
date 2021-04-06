f27f9ed1efef34388b695aed4a34f874a30054ab
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 11:59:57 +0400

    test

--- cogs/starcraft.py
+++ cogs/starcraft.py
@@ -2,5 +2,9 @@ import discord
 from discord.ext import commands
 
 class StarCraft(commands.cog):
-    def __init__:
-        
+    def __init__(self):
+        self.bot = bot
+
+def setup(bot):
+    bot.add_cog(StarCraft(bot))
+
